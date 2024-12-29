046e95e187b00405ad51043438c691b20b5faa6a
Nisha <46855901+Nisha789@users.noreply.github.com>
2024-12-29 23:46:39 +0530

    Add file

---
+++ data/commands.sql
@@ -0,0 +1,29 @@
+-- Create Database
+create database customer;
+
+-- use database
+use customer;
+
+-- use schema
+create schema target;
+
+-- create table
+create table target.customer_csv (
+	customer_pk number(38,0),
+	salutation varchar(10),
+	first_name varchar(20),
+	last_name varchar(30),
+	gender varchar(1),
+	marital_status varchar(1),
+	day_of_birth date,
+	birth_country varchar(60),
+	email_address varchar(50),
+	city_name varchar(60),
+	zip_code varchar(10),
+	country_name varchar(20),
+	gmt_timezone_offset number(10,2),
+	preferred_cust_flag boolean,
+	registration_time timestamp_ltz(9)
+);
+
+select * from customer_csv;
